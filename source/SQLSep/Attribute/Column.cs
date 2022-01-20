53ddba826b9cdfcbe27f500b258662ec1548c033
nhlong20 <hoanglongmcs1@gmail.com>
2022-01-20 20:02:30 +0700

    feat: attribute

---
+++ source/SQLSep/Attribute/Column.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEPFramework.source.SQLSep.Attribute
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class Column : EntityAttribute
+    {
+        private string _name;
+
+        public Column(String name)
+        {
+            this._name = name;
+        }
+
+        public static string getName(PropertyInfo prop)
+        {
+            return ((Column)prop.GetCustomAttribute(typeof(Column)))._name;
+        }
+
+    }
+}
---
+++ source/SQLSep/SepDataType/DataType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEPFramework.source.SQLSep.SepDataType
+{
+    public interface DataType
+    {
+        string GenCreateQuery(PropertyInfo prop);
+        string GenSQLValue(object value);
+    }
+}
